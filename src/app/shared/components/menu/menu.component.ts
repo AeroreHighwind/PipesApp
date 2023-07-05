d04914e1fadfa211872aa540c359688e93de7bb8
Gaston Gallegos <gaston52343@gmail.com>
2023-07-05 13:33:39 -0300

    MenuBar implemented from PrimeNG

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: [],
+})
+export class MenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items : [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          }
+        ]
+      },
+      { label: 'Undo', icon: 'pi pi-fw pi-refresh' },
+    ];
+  }
+}
