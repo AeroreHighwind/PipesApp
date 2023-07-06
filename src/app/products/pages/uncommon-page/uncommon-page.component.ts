bc87a93e1f1913f14a6b55ba1175edbc59efca88
Gaston Gallegos <gaston52343@gmail.com>
2023-07-06 09:16:45 -0300

    i18n pipes implemented

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Phartax';
+  public gender : 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male' : 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients : string[] = ['Maria', 'Pedro','Fernando','Eduardo','Melissa','Nat','Eve'];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': ' tenemos un cliente esperando',
+    'other': ' tenemos # clientes esperando',
+
+  }
+
+  deleteClient():void{
+    this.clients.shift();
+  }
+
+
+
 }
