df5a1f822c61fc9dbae45defa306bae223af527c
Gaston Gallegos <gaston52343@gmail.com>
2023-07-06 16:18:56 -0300

    Custom pipe created, order component added

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -5,6 +5,8 @@ import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
 import { PanelModule } from 'primeng/panel';
+import {ToolbarModule} from 'primeng/toolbar';
+
 
 @NgModule({
   exports: [
@@ -12,7 +14,8 @@ import { PanelModule } from 'primeng/panel';
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    ToolbarModule
   ],
 })
 
